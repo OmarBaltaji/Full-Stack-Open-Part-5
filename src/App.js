9cf0b0759c7ad6856c2c0f98fdc80147e5fa9ba6
Omar Baltaji <omr.baltaji@gmail.com>
2023-07-17 00:25:41 +0300

    bloglist frontend, step1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +13,22 @@ const App = () => {
     )  
   }, [])
 
+  const postLogin = (user) => {
+   setUser(user);
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {!user && <LoginForm postLogin={postLogin} />}
+      {user &&
+        <>
+          <h2>blogs</h2>
+          <p>Logged in as {user.name}</p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </>
+      }
     </div>
   )
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import loginService from '../services/login';
+
+const LoginForm = ({ postLogin }) => {
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (target, field) => {
+    setCredentials(oldCredentials => ({ ...oldCredentials, [field]: target.value }));
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    const user = await loginService.login(credentials);
+    postLogin(user);
+  }
+
+  
+  return (
+    <>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>username</label>
+          <input name="username" value={credentials.username} onChange={({ target }) => handleChange(target, 'username')}  />
+        </div>
+        <div>
+          <label>password</label>
+          <input type="password" name="password" value={credentials.password} onChange={({ target }) => handleChange(target, 'password')} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  )
+}
+
+export default LoginForm;
