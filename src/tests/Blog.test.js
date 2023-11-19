da6ceddcb880b91b630ae04e682caaf06ba08cfa
Omar Baltaji <omr.baltaji@gmail.com>
2023-11-19 19:41:31 +0200

    Blog list tests, step2

--- src/tests/Blog.test.js
+++ src/tests/Blog.test.js
@@ -2,6 +2,8 @@ import Reach from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Blog from '../components/Blog';
+import userEvent from '@testing-library/user-event';
+import 'regenerator-runtime/runtime';
 
 test('renders content', () => {
   const onLikeClicked = jest.fn();
@@ -22,4 +24,26 @@ test('renders content', () => {
 
   expect(element).toHaveTextContent(`${blog.title} ${blog.author}`);
   expect(element).not.toHaveTextContent(`${blog.url} ${blog.likes}`);
+});
+
+test('after clicking the button, url and likes show', async () => {
+  const onLikeClicked = jest.fn();
+  const onDeleteBlog = jest.fn();
+  const user = { id: 1, username: 'test', name: 'test' };
+  const blog = {
+    title: 'test title',
+    author: 'random author',
+    url: 'https://randomurl.com',
+    likes: 0,
+    user: user
+  };
+
+  const { container } = render(<Blog blog={blog} onLikeClicked={onLikeClicked} onDeleteBlog={onDeleteBlog} user={user} />);
+
+  const userEventInstance = userEvent.setup();
+  const button = screen.getByText('View');
+  await userEventInstance.click(button);
+
+  const element = container.querySelector('.blog');
+  expect(element).toHaveTextContent(`${blog.url}${blog.likes}`);
 });
